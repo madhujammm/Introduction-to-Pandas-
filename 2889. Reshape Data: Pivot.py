06bb4488df33877b89ddcf00b66c40d99e47ff57
madhujammm <155441219+madhujammm@users.noreply.github.com>
2025-02-27 10:19:56 +0530

    Update 2889. Reshape Data: Pivot.py

--- 2889. Reshape Data: Pivot.py
+++ 2889. Reshape Data: Pivot.py
@@ -1,5 +1,4 @@
 import pandas as pd
 
 def pivotTable(weather: pd.DataFrame) -> pd.DataFrame:
-    return weather.pivot(index="month", columns="city", values="temperature").reset_index()
-
+    return weather.pivot(index='month', columns='city', values='temperature')
