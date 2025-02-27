fb4d2c73b34c383bb68cdd380c2c55a1dff7fb38
madhujammm <155441219+madhujammm@users.noreply.github.com>
2025-02-27 10:16:41 +0530

    Update 2888. Reshape Data: Concatenate.py

--- 2888. Reshape Data: Concatenate.py
+++ 2888. Reshape Data: Concatenate.py
@@ -1,1 +1,15 @@
 
+#m1
+import pandas as pd
+
+def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+    df1 = pd.DataFrame(df1, columns=['student_id', 'name', 'age'])
+    df2 = pd.DataFrame(df2, columns=['student_id', 'name', 'age'])
+    frames = [df1,df2]
+    return pd.concat(frames)
+
+#m2
+import pandas as pd
+
+def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+    return pd.concat([df1, df2], ignore_index=True)
